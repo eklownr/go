9a718e481b54942ace09671611c9dccfd16d9a16
eklownr <eklownr@gmail.com>
2024-10-31 08:16:46 -0700

    receiving map

--- hello/hello.go
+++ hello/hello.go
@@ -13,12 +13,45 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(11)
 
-	check_for_error("go Golang go")
-	check_for_error("") // Hello("") will return an error
+	multi_greetings()
+	hello_name("Golang")
+	//hello_name("") // Hello("") will return an error
+
+	// receiving map
+	my_map := get_name_map()
+	fmt.Println(my_map)
+	// print key and value
+	for k, v := range my_map {
+		fmt.Printf("key: %v. value: %v \n", k, v)
+	}
 }
 
-func check_for_error(s string) {
-	message, err := greetings.Hello(s)
+func multi_greetings() {
+	// A slice of names.
+	names := []string{"Gladys", "Samantha", "Darrin"}
+
+	// Request greeting messages for the names.
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// If no error was returned, print the returned map of
+	// messages to the console.
+	fmt.Println(messages)
+}
+
+func get_name_map() map[string]string {
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return messages
+
+}
+
+func hello_name(name string) {
+	message, err := greetings.Hello(name)
 	// If an error was returned, print it to the console and exit the program.
 	if err != nil {
 		log.Fatal(err)
