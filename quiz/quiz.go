68ee1de40c7fc14cb2a973170b17505257466ac6
eklownr <eklownr@gmail.com>
2024-11-01 05:15:36 -0700

    quiz game

---
+++ quiz/quiz.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	csvFileName := flag.String("csv", "problems.cvs", "A csv file in format of 'qestion, answer'")
+	flag.Parse()
+
+	file, err := os.Open(*csvFileName)
+	if err != nil {
+		exit(fmt.Sprintf("Failed to open CSV file: %s\n", *csvFileName))
+	}
+	r := csv.NewReader(file)
+	lines, err := r.ReadAll()
+	if err != nil {
+		exit("Failed to parse the provied csv file.")
+	}
+
+	problems := parseLines(lines)
+	for i, p := range problems {
+		fmt.Printf("Problem #%d: %s", i+1, p.q)
+	}
+
+}
+
+func parseLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, line := range lines {
+		ret[i] = problem{
+			q: line[0],
+			a: line[1],
+		}
+	}
+	return ret
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func exit(msg string) {
+	fmt.Println(msg)
+	os.Exit(1)
+}
+
+func pl(s string) {
+	fmt.Println(s)
+}
