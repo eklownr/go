f1925f9f984e58f4f768220c9692e73f7c895a81
eklownr <eklownr@gmail.com>
2024-11-09 15:16:20 +0100

    more testing

--- basic/reverseWords.go
+++ basic/reverseWords.go
@@ -5,12 +5,25 @@ import (
 	"strings"
 )
 
+func ReverseWords2(str string) string {
+	strSplit := strings.Split(str, " ")
+	var res []string
+	for i := len(strSplit) - 1; i >= 0; i-- {
+		res = append(res, strSplit[i])
+	}
+	return strings.Join(res, " ")
+}
+
 func ReverseWords(str string) string {
 	var rw string
 	words := strings.Split(str, " ")
 	for i, _ := range words {
 		i++
-		rw += words[len(words)-i] + " "
+		rw += words[len(words)-i]
+
+		if len(words)-i != 0 {
+			rw += " "
+		}
 	}
 	return rw // reversed words
 }
@@ -27,8 +40,13 @@ func reverseRunes(word string) string {
 }
 
 func main() {
-	w := ReverseWords("hello world here I come")
+	w := ReverseWords("hello world here i come")
 	fmt.Println(w)
+	fmt.Print(w)
+	fmt.Print(w)
 	rr := reverseRunes("world")
 	fmt.Println(rr)
+	rw := ReverseWords2("hello world here i come")
+	fmt.Print(rw)
+	fmt.Println(rw)
 }
