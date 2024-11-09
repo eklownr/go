fc65c19dc7a8a07d46d3b2c3abe2ef33809da855
eklownr <eklownr@gmail.com>
2024-11-09 12:45:54 +0100

    reverseRunes

--- basic/reverseWords.go
+++ basic/reverseWords.go
@@ -1,21 +1,34 @@
 package main
 
 import (
-  "fmt"
-  "strings"
+	"fmt"
+	"strings"
 )
 
 func ReverseWords(str string) string {
-  var rw  string
-  words:= strings.Split(str, " ")
-  for i, w := range words {
-    fmt.Printf("{i: %v,  w: %v\n", i, w)
-    rw += w[len(words)-i]
-  }
-  return rw // reverse those words
+	var rw string
+	words := strings.Split(str, " ")
+	for i, _ := range words {
+		i++
+		rw += words[len(words)-i] + " "
+	}
+	return rw // reversed words
 }
 
-func main () {
-  w := ReverseWords("hello world here I come")
-  fmt.Println(w)
+func reverseRunes(word string) string {
+	var rr string
+	for i, _ := range word {
+		//fmt.Printf("i: %v, r: %v \n", i, r)
+		i++
+		rr += string(word[len(word)-i])
+
+	}
+	return rr
+}
+
+func main() {
+	w := ReverseWords("hello world here I come")
+	fmt.Println(w)
+	rr := reverseRunes("world")
+	fmt.Println(rr)
 }
