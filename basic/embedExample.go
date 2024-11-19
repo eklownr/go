f855d7cff2bdb87f4d3dbf29a83f6572ece9bd10
eklownr <eklownr@gmail.com>
2024-11-19 16:02:27 +0100

    embed.FS

--- basic/embedExample.go
+++ basic/embedExample.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"embed"
 	_ "embed"
 	"fmt"
 )
@@ -10,7 +11,16 @@ var (
 	data string
 )
 
+//go:embed *.go
+var pwd embed.FS
+
 func main() {
 	fmt.Println(data)
 
+	fmt.Println("All go files in this directory:")
+
+	files, _ := pwd.ReadDir(".")
+	for _, file := range files {
+		fmt.Println(file.Name())
+	}
 }
