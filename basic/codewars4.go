549fbcd31a995464525333d992a3f11955827b9f
eklownr <eklownr@gmail.com>
2024-11-21 12:27:08 +0100

    NearestSq

--- basic/codewars4.go
+++ basic/codewars4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strings"
 )
 
@@ -10,9 +11,28 @@ type MyStr string
 func (s MyStr) IsUpperCase() bool {
 	return s == MyStr(strings.ToUpper(string(s)))
 }
+
+func Derive(coefficient, exponent int) string {
+	return fmt.Sprintf("%dx^%d", coefficient*exponent, exponent-1)
+}
+
+func NearestSq(n int) int {
+	root := math.Sqrt(float64(n))
+
+	if int((root)+1)*(int(root)+1)-n < n-int(root)*int(root) {
+		return int((root)+1) * (int(root) + 1)
+	}
+	return int(root) * int(root)
+}
+
 func main() {
 	fmt.Println("Hello play ground")
 	fmt.Println(MyStr.IsUpperCase("c"))
 	fmt.Println(MyStr.IsUpperCase("C"))
 	fmt.Println(MyStr.IsUpperCase("ABC"))
+
+	fmt.Println(NearestSq(9))
+	fmt.Println(NearestSq(10))
+	fmt.Println(NearestSq(8))
+	fmt.Println(NearestSq(111))
 }
