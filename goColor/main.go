acc20d260a24717317c3b0b879032fed7467bff3
eklownr <eklownr@gmail.com>
2024-11-22 21:47:41 +0100

    fatih

--- goColor/main.go
+++ goColor/main.go
@@ -19,8 +19,32 @@ func main() {
 	red := color.New(color.FgRed).PrintfFunc()
 	red("Warning\n")
 
-	pl := color.New(color.FgHiGreen).PrintfFunc()
-	p := color.New(color.FgHiBlue).PrintfFunc()
-	pl("This message is green new line \n")
-	p("This message is blue")
+	//blue := color.New(color.FgHiBlue).PrintfFunc()
+	pl("This message is cyan and a new line ")
+	pl("This message is cyan with extra string %s", "...extra...")
+}
+
+// replace for fmt.Printf(string, multiple values)
+func pl(s string, value ...interface{}) {
+	green := color.New(color.FgHiGreen).PrintfFunc()
+	cyan := color.New(color.FgHiCyan).PrintfFunc()
+	i := len(value)
+	switch i {
+	case 0:
+		cyan(s + "\n")
+	case 1:
+		cyan(s+"\n", value[0])
+	case 2:
+		cyan(s+"\n", value[0], value[1])
+	case 3:
+		cyan(s+"\n", value[0], value[1], value[2])
+	case 4:
+		cyan(s+"\n", value[0], value[1], value[2], value[3])
+	case 5:
+		cyan(s+"\n", value[0], value[1], value[2], value[3], value[4])
+	case 6:
+		cyan(s+"\n", value[0], value[1], value[2], value[3], value[4], value[5])
+	default:
+		green("Too many values")
+	}
 }
