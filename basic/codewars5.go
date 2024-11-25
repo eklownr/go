bac5d33ef2632c4d3762da9c95edc59c7b6ad338
eklownr <eklownr@gmail.com>
2024-11-25 23:54:53 +0100

    unicode

--- basic/codewars5.go
+++ basic/codewars5.go
@@ -4,31 +4,80 @@ import (
 	"fmt"
 	"strconv"
 	"strings"
+	"unicode"
 
 	"github.com/eklownr/pretty"
 )
 
+func Persistence2(n int) int {
+	steps := 0
+	for n >= 10 {
+		m := 1
+		for n > 0 {
+			m *= n % 10
+			n /= 10
+		}
+		n = m
+		steps++
+	}
+	return steps
+}
+
 func Persistence(n int) int {
 	count := 0
-	digits := strings.Split(strconv.Itoa(n), "")
-	intSlice := make([]int, len(digits))
-	for i, d := range digits {
-		intSlice[i], _ = strconv.Atoi(d)
-	}
-	fmt.Println(intSlice) // [1, 2, 3, 4]
+	prod := 1
 
-	// // slice := []int{}
-	// for i := n; i > 10; i %= 10 {
-	// 	count++
-	// }
+	for n >= 10 {
+		strOfiInt := strings.Split(strconv.Itoa(n), "")
+		intSlice := make([]int, len(strOfiInt))
+		for i, d := range strOfiInt {
+			intSlice[i], _ = strconv.Atoi(d)
+		}
+		//fmt.Println(intSlice) // TEST
+		for _, v := range intSlice {
+			prod *= v
+		}
+		count++
+		n = prod
+		prod = 1
+		//p("prod: %v", prod) // TEST
+	}
 	return count
 }
 
+func Gimme(array [3]int) int {
+	switch {
+	case array[0] > array[1] && array[0] < array[2]:
+		return 0
+	case array[0] > array[2] && array[0] < array[1]:
+		return 0
+	case array[1] > array[0] && array[1] < array[2]:
+		return 1
+	case array[1] > array[2] && array[1] < array[0]:
+		return 1
+	}
+	return 2
+}
+
+func ToAlternatingCase(str string) string {
+	runes := []rune(str)
+	s := ""
+	for _, r := range runes {
+		if unicode.IsUpper(r) {
+			s += string(unicode.ToLower(r))
+		} else {
+			s += string(unicode.ToUpper(r))
+		}
+	}
+	return s
+}
+
 func main() {
-	p("39 gives %d", Persistence(39))
-	p("999 gives %d", Persistence(999))
-	p("4 gives %d", Persistence(4))
-	p("25 gives %d", Persistence(25))
+	p(ToAlternatingCase("aA bB HELLO hello"))
+	// p("39 gives %d expect: %d", Persistence(39), 3)
+	// p("999 gives %d expect: %d", Persistence(999), 4)
+	// p("4 gives %d expect: %d", Persistence(4), 0)
+	// p("25 gives %d expect: %d", Persistence(25), 2)
 
 	// pln("***********************", "red")
 	// p("Hello, World! ( 1234 ) 1 + 1 = 2")
